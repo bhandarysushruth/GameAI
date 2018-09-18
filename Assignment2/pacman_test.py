4809f921d3b6332c06eb80ee2c434bd9fed0acf7
Sushruth <sushrut.bhandary@gmail.com>
2018-09-18 15:33:13 -0700

    converted to individual funcs. part 3 done

--- Assignment2/pacman_test.py
+++ Assignment2/pacman_test.py
@@ -48,10 +48,77 @@ def printMsg(text_string,x,y):
     text = font.render(text_string, True, (0, 128, 0))
     gameDisplay.blit(text,(x, y))
 
+## takes in the cordinates of the pacman and monster and the monster velocity and returns the angular velocities
+def seek(x,y,mx,my,mvel):
 
-#takes the locations of the monster and player and tells the monster which direction to move in
-def calc_direction(mx,my,px,py):
-    return 1
+    if (x - mx) == 0:
+        if (y - my) > 0:
+            theta = math.radians(90)
+        elif (y - mon_y) < 0:
+            theta = math.radians(-90)
+        else:
+            theta = math.radians(0)
+    
+    else : 
+
+        tanVal = (y - my)/(x - mx)
+        theta = (math.atan(tanVal))
+    
+    
+
+    # right half :
+    if (mx > x):
+        mon_vel_x = mvel * math.cos(theta) * -1
+        mon_vel_y = mvel * math.sin(theta) * -1
+    
+    ## left half
+    else:
+
+        mon_vel_x = mvel * math.cos(theta)
+        mon_vel_y = mvel * math.sin(theta)    
+
+    return mon_vel_x, mon_vel_y
+
+def flee(x,y,mx,my,mvel):
+
+    if calcDistance(x,y,mx,my) < 200:
+
+        ## divide by zero errors
+
+        if (x - mx) == 0:
+            if (y - my) > 0:
+                theta = math.radians(90)
+            elif (y - my) < 0:
+                theta = math.radians(-90)
+            else:
+                theta = math.radians(0)
+        
+        else : 
+
+            tanVal = (y - my)/(x - mx)
+            theta = (math.atan(tanVal))
+        
+        
+
+        # right half :
+        if (mx > x):
+            mon_vel_x = mvel * math.cos(theta)
+            mon_vel_y = mvel * math.sin(theta)
+        
+        ## left half
+        else:
+
+            mon_vel_x = mvel * math.cos(theta) * -1
+            mon_vel_y = mvel * math.sin(theta) * -1          
+
+
+    else :
+        ## monster stays in place if its more than 200 pixels away
+        mon_vel_x = 0
+        mon_vel_y = 0
+
+
+    return mon_vel_x, mon_vel_y
 
 def calcDistance(x1,y1,x2,y2):
     val = (x2-x1)**2
@@ -71,14 +138,14 @@ if __name__ == '__main__':
     #pacman attributes
     x_change = 0
     y_change = 0
-    x = 300
-    y = 500
+    x = 100
+    y = 100
     
 
     #monster attributes
     theta = 0
-    mon_x = 700
-    mon_y = 600
+    mon_x = 400
+    mon_y = 400
     mon_vel = 3
     mon_vel_x = 0
     mon_vel_y = 0
@@ -133,91 +200,33 @@ if __name__ == '__main__':
         ## making the monster stay inside the game window
 
         
-        if args.monsterBehavior == 'seek' or args.monsterBehavior == 'chase':
-            
-            # taking care of divide by zero errors
+        if args.monsterBehavior == 'seek':
+    
+            mon_vel_x, mon_vel_y = seek(x,y,mon_x,mon_y,mon_vel)
 
-            if (x - mon_x) == 0:
-                if (y - mon_y) > 0:
-                    theta = math.radians(90)
-                elif (y - mon_y) < 0:
-                    theta = math.radians(-90)
-                else:
-                    theta = math.radians(0)
-            
-            else : 
-
-                tanVal = (y - mon_y)/(x - mon_x)
-                theta = (math.atan(tanVal))
-            
-            
-
-            # right half :
-            if (mon_x > x):
-                mon_vel_x = mon_vel * math.cos(theta) * -1
-                mon_vel_y = mon_vel * math.sin(theta) * -1
-            
-            ## left half
-            else:
-
-                mon_vel_x = mon_vel * math.cos(theta)
-                mon_vel_y = mon_vel * math.sin(theta)
-
-            '''
-            if (mon_x + mon_vel_x) > display_width or (mon_x + mon_vel_x) < 0:
-                mon_vel_x = mon_vel_x * -1
-
-            if (mon_y + mon_vel_y) > display_height or (mon_y + mon_vel_y) < 0:
-                mon_vel_y = mon_vel_y * -1
-            '''
-            
             mon_x = mon_x + mon_vel_x
             mon_y = mon_y + mon_vel_y
 
+        elif args.monsterBehavior == 'chase':
+            
+            #chase is basically seek but for a future position
+            mon_vel_x, mon_vel_y = seek(x+x_change, y+y_change, mon_x, mon_y, mon_vel)
+            mon_x = mon_x + mon_vel_x
+            mon_y = mon_y + mon_vel_y
         
         elif args.monsterBehavior == 'flee':
             
             ## monster starts fleeing if pacman is within 200 pixels
-            if calcDistance(x,y,mon_x,mon_y) < 200:
-                
-                ## divide by zero errors
 
-                if (x - mon_x) == 0:
-                    if (y - mon_y) > 0:
-                        theta = math.radians(90)
-                    elif (y - mon_y) < 0:
-                        theta = math.radians(-90)
-                    else:
-                        theta = math.radians(0)
-                
-                else : 
-
-                    tanVal = (y - mon_y)/(x - mon_x)
-                    theta = (math.atan(tanVal))
-                
-                
-
-                # right half :
-                if (mon_x > x):
-                    mon_vel_x = mon_vel * math.cos(theta)
-                    mon_vel_y = mon_vel * math.sin(theta)
-                
-                ## left half
-                else:
-
-                    mon_vel_x = mon_vel * math.cos(theta) * -1
-                    mon_vel_y = mon_vel * math.sin(theta) * -1          
-
-
-            else :
-                ## monster stays in place if its more than 200 pixels away
-                mon_vel_x = 0
-                mon_vel_y = 0
+            mon_vel_x, mon_vel_y = flee(x, y, mon_x, mon_y,mon_vel)
 
             mon_x = mon_x + mon_vel_x
             mon_y = mon_y + mon_vel_y
 
-        
+            ## making the monster stay inside the windows
+            if (mon_x > display_width -25) or (mon_x < 5) or (mon_y < 5) or (mon_y > display_height - 25):
+                mon_x = mon_x - mon_vel_x
+                mon_y = mon_y - mon_vel_y                
 
         else:
 
